9c6b23fd59699e2a326a1e0155d2398c69c6d557
Mike Harris <mikeharris22@gmail.com>
2024-01-16 13:03:52 -0500

    Customized config for minisurround.

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -3,6 +3,21 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
     },
   },
 }
